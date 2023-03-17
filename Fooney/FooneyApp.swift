93c82f45679b44ebe8e07da419da458ba7935c61
Adryan Eka Vandra <adryanekavandra@gmail.com>
2023-03-18 01:09:10 +0700

    Initial Commit

---
+++ Fooney/FooneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FooneyApp.swift
+//  Fooney
+//
+//  Created by Adryan Eka Vandra on 18/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FooneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
